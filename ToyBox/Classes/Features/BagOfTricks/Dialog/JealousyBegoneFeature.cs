0733aaed4accd37480057e059ccc9aa114b15aad
xADDBx <62178123+xADDBx@users.noreply.github.com>
2025-11-02 21:34:31 +0100

    Jealousy; Love

--- ToyBox/Classes/Features/BagOfTricks/Dialog/JealousyBegoneFeature.cs
+++ ToyBox/Classes/Features/BagOfTricks/Dialog/JealousyBegoneFeature.cs
@@ -1,4 +1,8 @@
-﻿namespace ToyBox.Features.BagOfTricks.Dialog;
+﻿using Kingmaker.Designers.EventConditionActionSystem.Conditions;
+using Kingmaker.ElementsSystem;
+using Kingmaker.ElementsSystem.Interfaces;
+
+namespace ToyBox.Features.BagOfTricks.Dialog;
 
 [HarmonyPatch, ToyBoxPatchCategory("ToyBox.Features.BagOfTricks.Dialog.JealousyBegoneFeature")]
 public partial class JealousyBegoneFeature : FeatureWithPatch {
@@ -19,30 +23,59 @@ public partial class JealousyBegoneFeature : FeatureWithPatch {
     }
     #region Overrides
     // This modify the EtudeStatus condition for specific Owner blueprints 
-    private static readonly Dictionary<(string, string), bool> ConditionCheckOverrides = new() {
-            // Cue_43 (I assume Kibella) check for any active romance
-            { ("f4b42351c500429ba9cfbccf352ddd3b", "6a8601e5d98a450f8ed8b644c1cf1fea"), false },
-            // Cue_43 (I assume Kibella) check jealousy dialog seen or active romances = 1 
-            { ("f4b42351c500429ba9cfbccf352ddd3b", "09f80fe401704413864b879f3bdfb970"), false }
-        };
+    private static readonly Dictionary<(string, string), bool> m_ConditionCheckOverrides = new() {
+        // Cue_43 (I assume Kibella) check for any active romance
+        { ("f4b42351c500429ba9cfbccf352ddd3b", "6a8601e5d98a450f8ed8b644c1cf1fea"), false },
+        // Cue_43 (I assume Kibella) check jealousy dialog seen or active romances = 1 
+        { ("f4b42351c500429ba9cfbccf352ddd3b", "09f80fe401704413864b879f3bdfb970"), false }
+    };
     // This modifies all conditions of the blueprint with the specified id
-    private static readonly Dictionary<string, bool> AllConditionCheckOverrides = new() {
-            // Kibellah romance after coming back (All)
-            { "d269a5417ca646759584a2ab7bddf319", false }, { "5ca382ed53964851bd19ce07efb7bb8c", false },
-            // Kibellah romance after coming back (Cas)
-            { "683bf82b7663452a9fb92955b4b1d031", false }, { "aae351192ac24f84ab36e1839c1ab7c3", false },
-            // Kibellah romance after coming back (Hein)
-            { "6b08fa9121c54f3c811536fef69f12c9", false }, { "ec45728761064a27bfafca1fbfa65355", false },
-            // Kibellah romance after coming back (Jae)
-            { "25163b765a8442e2928f3423f080fa57", false }, { "83953390c5764c25a9d2d0b1f899f905", false },
-            // Kibellah romance after coming back (Mar)
-            { "7d30413d5b7a426aa891b1e282792134", false }, { "2b989a9de0f04c2c848269294a0a4452", false },
-            // Kibellah romance after coming back (Yrl)
-            { "c409b0626cce411ab6720916f310d9f2", false }, { "efc090e2c9794fac855be6c597637bda", false }
-        };
-    private static readonly Dictionary<string, bool> FlagInRangeOverrides = new() {
-            // RomanceCount Flag, as conditioned in Jealousy_event Blueprint, Activated by Jealousy_preparation
-            { "cbb219fcb46948fba48a8bed94663e5d", false }
-        };
+    private static readonly Dictionary<string, bool> m_AllConditionCheckOverrides = new() {
+        // Kibellah romance after coming back (All)
+        { "d269a5417ca646759584a2ab7bddf319", false }, { "5ca382ed53964851bd19ce07efb7bb8c", false },
+        // Kibellah romance after coming back (Cas)
+        { "683bf82b7663452a9fb92955b4b1d031", false }, { "aae351192ac24f84ab36e1839c1ab7c3", false },
+        // Kibellah romance after coming back (Hein)
+        { "6b08fa9121c54f3c811536fef69f12c9", false }, { "ec45728761064a27bfafca1fbfa65355", false },
+        // Kibellah romance after coming back (Jae)
+        { "25163b765a8442e2928f3423f080fa57", false }, { "83953390c5764c25a9d2d0b1f899f905", false },
+        // Kibellah romance after coming back (Mar)
+        { "7d30413d5b7a426aa891b1e282792134", false }, { "2b989a9de0f04c2c848269294a0a4452", false },
+        // Kibellah romance after coming back (Yrl)
+        { "c409b0626cce411ab6720916f310d9f2", false }, { "efc090e2c9794fac855be6c597637bda", false }
+    };
+    private static readonly Dictionary<string, bool> m_FlagInRangeOverrides = new() {
+        // RomanceCount Flag, as conditioned in Jealousy_event Blueprint, Activated by Jealousy_preparation
+        { "cbb219fcb46948fba48a8bed94663e5d", false }
+    };
     #endregion
+    [HarmonyPatch(typeof(Condition), nameof(Condition.Check), [typeof(ConditionsChecker), typeof(IConditionDebugContext)])]
+    public static class Condition_Check_Patch {
+        public static void Postfix(Condition __instance, ref bool __result) {
+            if (__instance.Owner is not null) {
+                if (m_AllConditionCheckOverrides.TryGetValue(__instance.Owner.AssetGuid, out var value)) {
+                    Debug($"Overiding {__instance.Owner.name} from {__result} to {value}");
+                    __result = value;
+                } else if (m_ConditionCheckOverrides.TryGetValue((__instance.Owner.AssetGuid, __instance.AssetGuid), out value)) {
+                    Debug($"Overiding {__instance.Owner.name} from {__result} to {value}");
+                    __result = value;
+                }
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(FlagInRange), nameof(FlagInRange.CheckCondition)), HarmonyPostfix]
+    public static void FlagInRange_CheckCondition_Patch(FlagInRange __instance, ref bool __result) {
+        if (__instance.Owner is not null) {
+            if (m_FlagInRangeOverrides.TryGetValue(__instance.Owner.AssetGuid, out var value)) {
+                Debug($"Overiding {__instance.Owner.name} from {__result} to {value}");
+                __result = value;
+            }
+        }
+    }
+    [HarmonyPatch(typeof(RomanceLocked), nameof(RomanceLocked.CheckCondition)), HarmonyPostfix]
+    public static void RomanceLocked_CheckCondition_Patch(ref bool __result) {
+        Debug($"Overiding RomanceLocked Condition from {__result} to false");
+        __result = false;
+    }
 }
--- ToyBox/Classes/Features/BagOfTricks/Dialog/LoveIsFreeFeature.cs
+++ ToyBox/Classes/Features/BagOfTricks/Dialog/LoveIsFreeFeature.cs
@@ -20,64 +20,63 @@ public partial class LoveIsFreeFeature : FeatureWithPatch {
         }
     }
     #region Overrides
-    // Any Gender Any Romance Overrides
     // These modify the PcFemale/PcMale conditions for specific Owner blueprints 
     private static readonly Dictionary<string, bool> m_PcFemaleOverrides = new() {
-            // World\Dialogs\Companions\Romances\Heinrix\StartingEvent\Answer_0004
-            { "5457755c30ac417d9279fd740b90f549", true },
-            // World\Dialogs\Companions\Romances\Heinrix\StartingEvent\Answer_0023
-            { "8d6b7c53af134494a64a4de789759fb9", true },
-            // World\Dialogs\Companions\Romances\Heinrix\StartingEvent\Answer_11
-            { "4b4b769261f04a8cb5726e111c3f7081", true },
-            // World\Dialogs\Companions\Romances\Heinrix\StartingEvent\Answer_2
-            { "cf1d7205cf854709b038db477db48ac9", true },
-            // World\Dialogs\Companions\Romances\Heinrix\StartingEvent\Check_0011
-            { "d2c500fbc1b5450c8663d453a33b0eee", true },
-            // Dialog Blueprints which contain the PcMale override but seem not directly related to romance
-            // World\Dialogs\Ch1\BridgeAndCabinet\Briefing\Answer_15
-            { "02e0bc30b5a146708dd62d68ac7490bd", true },
-            // World\Dialogs\Companions\CompanionDialogues\Interrogator\Cue_10
-            { "2df6bd21ad5a45a9b1c5142d51d647dc", true },
-            // World\Dialogs\Companions\CompanionDialogues\Navigator\Cue_45
-            { "0739ef639d774629a27d396cd733cfd4", true },
-            // World\Dialogs\Companions\CompanionDialogues\Navigator\Cue_67
-            { "ea42722c44c84835b7363db2fc09b23b", true },
-            // World\Dialogs\Companions\CompanionDialogues\Ulfar\Cue_47
-            { "41897fd7a52249d3a53691fbcfcc9c19", true },
-            // World\Dialogs\Companions\CompanionDialogues\Ulfar\Cue_89
-            { "c5efaa0ace544ca7a81d439e7cfc6ae5", true }
-        };
+        // World\Dialogs\Companions\Romances\Heinrix\StartingEvent\Answer_0004
+        { "5457755c30ac417d9279fd740b90f549", true },
+        // World\Dialogs\Companions\Romances\Heinrix\StartingEvent\Answer_0023
+        { "8d6b7c53af134494a64a4de789759fb9", true },
+        // World\Dialogs\Companions\Romances\Heinrix\StartingEvent\Answer_11
+        { "4b4b769261f04a8cb5726e111c3f7081", true },
+        // World\Dialogs\Companions\Romances\Heinrix\StartingEvent\Answer_2
+        { "cf1d7205cf854709b038db477db48ac9", true },
+        // World\Dialogs\Companions\Romances\Heinrix\StartingEvent\Check_0011
+        { "d2c500fbc1b5450c8663d453a33b0eee", true },
+        // World\Dialogs\Ch1\BridgeAndCabinet\Briefing\Answer_15; Override not necessary as it's unrelated to romance
+        // { "02e0bc30b5a146708dd62d68ac7490bd", true },
+        // World\Dialogs\Companions\CompanionDialogues\Interrogator\Cue_10
+        { "2df6bd21ad5a45a9b1c5142d51d647dc", true },
+        // World\Dialogs\Companions\CompanionDialogues\Navigator\Cue_45; Override set to false as we want the male response from Cassia
+        { "0739ef639d774629a27d396cd733cfd4", false },
+        // World\Dialogs\Companions\CompanionDialogues\Navigator\Cue_67; Override not necessary as both male and female responses are fitting
+        // { "ea42722c44c84835b7363db2fc09b23b", true },
+        // World\Dialogs\Companions\CompanionDialogues\Ulfar\Cue_47M Override not necessary as it's unrelated to romance
+        // { "41897fd7a52249d3a53691fbcfcc9c19", true },
+        // World\Dialogs\Companions\CompanionDialogues\Ulfar\Cue_89; Override is not necessary as Cue is unreferenced and empty
+        // { "c5efaa0ace544ca7a81d439e7cfc6ae5", true }
+    };
     private static readonly Dictionary<string, bool> m_PcMaleOverrides = new() {
-            // World\Dialogs\Companions\Romances\Cassia\StartingEvent\Answer_0017
-            { "85b651edb4f74381bbe762999273c6ec", true },
-            // World\Dialogs\Companions\Romances\Cassia\StartingEvent\Answer_10
-            { "56bbf1612e05489ba44bb4a52718e222", true },
-            // World\Dialogs\Companions\Romances\Cassia\StartingEvent\Answer_5
-            { "eb76f93740824d16b1e1f54b82de21e0", true },
-            // World\Dialogs\Companions\Romances\Cassia\StartingEvent\Answer_8
-            { "c292b399f4344a639ccb4df9ba66329e", true },
-            // World\Dialogs\Companions\Romances\Cassia\StartingEvent\CassFirstTimeBlushing_a
-            { "95b0ba7d08e34f6c895b2fbeb53ea404", true },
-            // Dialog Blueprints which contain the PcMale override but seem not directly related to romance
-            // Dialogs\Companions\CompanionQuests\Navigator\Navigator_Q1\CassiaSeriousTalk\Answer_8
-            { "966f0cc2defa42bd836950aa1ebcde72", true },
-            // World\Dialogs\Companions\CompanionDialogues\Navigator\Cue_24
-            { "a903589840ba4ab683d6e6b9f985d458", true },
-            // World\Dialogs\Ch3\Chasm\PitCassia\Answer_11
-            { "c051d0c9f2ba4c23bff1d1e6f2cfe13d", true },
-            // World\Dialogs\Ch3\Chasm\PitCassia\Answer_12
-            { "3d24df76aacf4e2db047cf47ef3474d5", true },
-            // World\Dialogs\Ch3\Chasm\PitCassia\Answer_19
-            { "b3601cd9e84d43dbb4078bf77c89d728", true },
-            // World\Dialogs\Ch3\Chasm\PitCassia\Answer_6
-            { "17b34e1ae36443408805af3a3c2866f7", true },
-            // World\Dialogs\Ch3\Chasm\PitCassia\Cue_29
-            { "7f71e0b93dd9420d87151fc3e7114865", true },
-            // World\Dialogs\Companions\CompanionDialogues\Navigator\Cue_47
-            { "588a3c2e96c6403ca2c7104949b066e4", true },
-            // World\Dialogs\Companions\CompanionQuests\Navigator\Navigator_Q2\Cassia_Q2_BE\Cue_0037
-            { "bf7813b4ee3d49cdbc6305f454479db3", true }
-        };
+        // World\Dialogs\Companions\Romances\Cassia\StartingEvent\Answer_0017
+        { "85b651edb4f74381bbe762999273c6ec", true },
+        // World\Dialogs\Companions\Romances\Cassia\StartingEvent\Answer_10
+        { "56bbf1612e05489ba44bb4a52718e222", true },
+        // World\Dialogs\Companions\Romances\Cassia\StartingEvent\Answer_5
+        { "eb76f93740824d16b1e1f54b82de21e0", true },
+        // World\Dialogs\Companions\Romances\Cassia\StartingEvent\Answer_8
+        { "c292b399f4344a639ccb4df9ba66329e", true },
+        // World\Dialogs\Companions\Romances\Cassia\StartingEvent\CassFirstTimeBlushing_a
+        { "95b0ba7d08e34f6c895b2fbeb53ea404", true },
+        // Dialogs\Companions\CompanionQuests\Navigator\Navigator_Q1\CassiaSeriousTalk\Answer_8
+        { "966f0cc2defa42bd836950aa1ebcde72", true },
+        // World\Dialogs\Companions\CompanionDialogues\Navigator\Cue_24; Override not necessary as both male and female responses are fitting
+        // { "a903589840ba4ab683d6e6b9f985d458", true },
+        // World\Dialogs\Ch3\Chasm\PitCassia\Answer_11
+        { "c051d0c9f2ba4c23bff1d1e6f2cfe13d", true },
+        // World\Dialogs\Ch3\Chasm\PitCassia\Answer_12
+        { "3d24df76aacf4e2db047cf47ef3474d5", true },
+        // World\Dialogs\Ch3\Chasm\PitCassia\Answer_19
+        { "b3601cd9e84d43dbb4078bf77c89d728", true },
+        // World\Dialogs\Ch3\Chasm\PitCassia\Answer_6
+        { "17b34e1ae36443408805af3a3c2866f7", true },
+        // World\Dialogs\Ch3\Chasm\PitCassia\Cue_29
+        { "7f71e0b93dd9420d87151fc3e7114865", true },
+        // World\Dialogs\Companions\CompanionDialogues\Navigator\Cue_47
+        { "588a3c2e96c6403ca2c7104949b066e4", true },
+        // World\Dialogs\Companions\CompanionQuests\Navigator\Navigator_Q2\Cassia_Q2_BE\Cue_0037
+        { "bf7813b4ee3d49cdbc6305f454479db3", true },
+        // World\Dialogs\Epilogues\Cue_378
+        { "4d285cc256004e968d29730c5f9b2a5f", true }
+    };
     #endregion
     [HarmonyPatch(typeof(PcFemale), nameof(PcFemale.CheckCondition)), HarmonyPostfix]
     private static void PcFemale_CheckCondition_Patch(PcFemale __instance, ref bool __result) {
