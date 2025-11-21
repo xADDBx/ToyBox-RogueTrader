80ebfe2fa5025fb3a01ef5d195d5e32439e64e86
xADDBx <62178123+xADDBx@users.noreply.github.com>
2025-11-21 16:42:43 +0100

    LevelUpFeatureTab

---
+++ ToyBox/Classes/Features/LevelUp/LevelUpFeatureTab.cs
@@ -0,0 +1,9 @@
+﻿namespace ToyBox.Features.LevelUp;
+
+public partial class LevelUpFeatureTab : FeatureTab {
+    [LocalizedString("ToyBox_Features_LevelUp_LevelUpFeatureTab_Name", "Level Up")]
+    public override partial string Name { get; }
+    public LevelUpFeatureTab() {
+
+    }
+}
